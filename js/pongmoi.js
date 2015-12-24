521137ad899c39bd67d3eba67c67d39b0cccbbca
moisesjbc <moisesjbc@gmail.com>
2015-12-24 20:48:16 +0000

    Added paddles movement.

--- js/pongmoi.js
+++ js/pongmoi.js
@@ -1,20 +1,52 @@
 var game = new Phaser.Game(800, 600, Phaser.AUTO, '', { preload: preload, create: create, update: update });
 
+var paddle_1;
+var paddle_2;
+var top_border;
+var bottom_border;
+var paddle_speed = 7;
+
+var player_1_up;
+var player_1_down;
+var player_2_up;
+var player_2_down;
+
+
 function preload() 
 {
 }
 
+
 function create() 
 {
-    var paddle_1 = create_paddle(5, 50, 0x0000FF);
-    var paddle_2 = create_paddle(770, 50, 0xFF0000);
-    var top_border = create_horizontal_border(0);
-    var bottom_border = create_horizontal_border(580);
+    paddle_1 = create_paddle(5, 50, 0x0000FF);
+    paddle_2 = create_paddle(770, 50, 0xFF0000);
+    top_border = create_horizontal_border(0);
+    bottom_border = create_horizontal_border(580);
+
+    // Set inputs
+    player_1_up = game.input.keyboard.addKey(Phaser.Keyboard.W);
+    player_1_down = game.input.keyboard.addKey(Phaser.Keyboard.S);
+    player_2_up = game.input.keyboard.addKey(Phaser.Keyboard.UP);
+    player_2_down = game.input.keyboard.addKey(Phaser.Keyboard.DOWN);
 }
 
 
 function update() 
 {
+    // Process player 1 input.
+    if (player_1_up.isDown){
+        paddle_1.y -= paddle_speed;
+    }else if (player_1_down.isDown){
+        paddle_1.y += paddle_speed;
+    }
+
+    // Process player 2 input.
+    if (player_2_up.isDown){
+        paddle_2.y -= paddle_speed;
+    }else if (player_2_down.isDown){
+        paddle_2.y += paddle_speed;
+    }
 }
 
 
