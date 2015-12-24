c90becbaf44703252af90907bfd0c850f2d9e062
moisesjbc <moisesjbc@gmail.com>
2015-12-24 20:41:51 +0000

    Added game paddles and borders.

--- js/pongmoi.js
+++ js/pongmoi.js
@@ -6,6 +6,10 @@ function preload()
 
 function create() 
 {
+    var paddle_1 = create_paddle(5, 50, 0x0000FF);
+    var paddle_2 = create_paddle(770, 50, 0xFF0000);
+    var top_border = create_horizontal_border(0);
+    var bottom_border = create_horizontal_border(580);
 }
 
 
@@ -14,3 +18,25 @@ function update()
 }
 
 
+function create_paddle(x, y, color)
+{
+    paddle = game.add.graphics(x,y);
+    paddle.beginFill(color, 1);
+    paddle.lineStyle(2, 0xFFFFFF, 1);
+    paddle.drawRect(0, 0, 25, 100);
+    paddle.endFill();
+
+    return paddle;
+}
+
+
+function create_horizontal_border(y)
+{
+    border = game.add.graphics(0,y);
+    border.beginFill(0xAAAAAA, 1);
+    border.lineStyle(2, 0xFFFFFF, 1);
+    border.drawRect(0, 0, 800, 20);
+    border.endFill();
+
+    return paddle;
+}
