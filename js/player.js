fe559b7bd10745adf861035ee817df42633c8cc7
moisesjbc <moisesjbc@gmail.com>
2015-12-25 16:26:23 +0000

    Separated Player logic in a new class "Player".

---
+++ js/player.js
@@ -0,0 +1,36 @@
+
+
+function Player(x, y, color)
+{
+    // Create the bitmap representing the player's paddle.
+    var paddle_bitmap = game.add.bitmapData(25, 100);
+    paddle_bitmap.ctx.rect(0, 0, 25, 100);
+    paddle_bitmap.ctx.fillStyle = color;
+    paddle_bitmap.ctx.fill();
+
+    // Bitmap can't have physics on its own, so we create a sprite as 
+    // container.
+    this.paddle = physics_elements.create(x, y, paddle_bitmap);
+
+    // Enable physics for sprite.
+    game.physics.arcade.enable(this.paddle);
+    this.paddle.body.enable = true;
+    this.paddle.body.setSize(25,100);
+}
+
+
+Player.prototype.process_input = function(up_button, down_button)
+{
+    if (up_button.isDown){
+        this.paddle.body.velocity.y = -paddle_speed;
+    }else if (down_button.isDown){
+        this.paddle.body.velocity.y = paddle_speed;
+    }
+}
+
+
+Player.prototype.update = function()
+{
+    // Prevent paddles to be moved in X.
+    this.paddle.body.velocity.x = 0;
+}
--- js/pongmoi.js
+++ js/pongmoi.js
@@ -1,8 +1,8 @@
 var game = new Phaser.Game(800, 600, Phaser.AUTO, '', { preload: preload, create: create, update: update });
 
 var physics_elements;
-var paddle_1;
-var paddle_2;
+var player_1;
+var player_2;
 var ball;
 var top_border;
 var bottom_border;
@@ -29,8 +29,8 @@ function create()
     physics_elements.enableBody = true;
     physics_elements.physicsBodyType = Phaser.Physics.ARCADE;
 
-    paddle_1 = create_paddle(5, 50, '#0000FF');
-    paddle_2 = create_paddle(770, 50, '#FF0000');
+    player_1 = new Player(5, 50, '#0000FF');
+    player_2 = new Player(770, 50, '#FF0000');
     ball = create_ball(400, 300, 150, 150);
     top_border = create_horizontal_border(0);
     bottom_border = create_horizontal_border(580);
@@ -47,22 +47,11 @@ function update()
 {
     game.physics.arcade.collide(physics_elements, physics_elements);
 
-    // Process player 1 input.
-    if (player_1_up.isDown){
-        paddle_1.body.velocity.y = -paddle_speed;
-    }else if (player_1_down.isDown){
-        paddle_1.body.velocity.y = paddle_speed;
-    }
-    
-    // Process player 2 input.
-    if (player_2_up.isDown){
-        paddle_2.body.velocity.y = -paddle_speed;
-    }else if (player_2_down.isDown){
-        paddle_2.body.velocity.y = paddle_speed;
-    }
+    player_1.process_input(player_1_up, player_1_down);
+    player_2.process_input(player_2_up, player_2_down);
 
-    // Prevent paddles to be moved in X.
-    paddle_1.body.velocity.x = paddle_2.body.velocity.x = 0;
+    player_1.update();
+    player_2.update();
 
     // Restart ball if it goes out of screen.
     if( ball.position.x < 0 || ball.position.x > 800 ){
@@ -80,22 +69,6 @@ function update()
 function collisionHandler (obj1, obj2)
 {
     game.stage.backgroundColor = '#992d2d';
-}
-
-
-function create_paddle(x, y, color)
-{
-    var paddle = game.add.bitmapData(25, 100);
-    paddle.ctx.rect(0, 0, 25, 100);
-    paddle.ctx.fillStyle = color;
-    paddle.ctx.fill();
-
-    var paddle_sprite = physics_elements.create(x, y, paddle);
-    game.physics.arcade.enable(paddle_sprite);
-    paddle_sprite.body.enable = true;
-    paddle_sprite.body.setSize(25,100);
-
-    return paddle_sprite;
 }
 
 
