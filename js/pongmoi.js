38cb87a959371560fcd92b1852503552e924e381
moisesjbc <moisesjbc@gmail.com>
2015-12-25 13:09:22 +0000

    Added physics to paddles movement.

--- js/pongmoi.js
+++ js/pongmoi.js
@@ -1,10 +1,11 @@
 var game = new Phaser.Game(800, 600, Phaser.AUTO, '', { preload: preload, create: create, update: update });
 
+var physics_elements;
 var paddle_1;
 var paddle_2;
 var top_border;
 var bottom_border;
-var paddle_speed = 7;
+var paddle_speed = 100;
 
 var player_1_up;
 var player_1_down;
@@ -19,8 +20,15 @@ function preload()
 
 function create() 
 {
-    paddle_1 = create_paddle(5, 50, 0x0000FF);
-    paddle_2 = create_paddle(770, 50, 0xFF0000);
+    // Enable physics
+    game.physics.startSystem(Phaser.Physics.ARCADE);
+
+    physics_elements = game.add.group();
+    physics_elements.enableBody = true;
+    physics_elements.physicsBodyType = Phaser.Physics.ARCADE;
+
+    paddle_1 = create_paddle(5, 50, '#0000FF');
+    paddle_2 = create_paddle(770, 50, '#FF0000');
     top_border = create_horizontal_border(0);
     bottom_border = create_horizontal_border(580);
 
@@ -34,41 +42,58 @@ function create()
 
 function update() 
 {
+    game.physics.arcade.collide(physics_elements, physics_elements);
+
     // Process player 1 input.
     if (player_1_up.isDown){
-        paddle_1.y -= paddle_speed;
+        paddle_1.body.velocity.y = -paddle_speed;
     }else if (player_1_down.isDown){
-        paddle_1.y += paddle_speed;
+        paddle_1.body.velocity.y = paddle_speed;
     }
 
     // Process player 2 input.
     if (player_2_up.isDown){
-        paddle_2.y -= paddle_speed;
+        paddle_2.body.velocity.y = -paddle_speed;
     }else if (player_2_down.isDown){
-        paddle_2.y += paddle_speed;
+        paddle_2.body.velocity.y = paddle_speed;
     }
+}
+
+
+function collisionHandler (obj1, obj2)
+{
+    game.stage.backgroundColor = '#992d2d';
 }
 
 
 function create_paddle(x, y, color)
 {
-    var paddle = game.add.graphics(x,y);
-    paddle.beginFill(color, 1);
-    paddle.lineStyle(2, 0xFFFFFF, 1);
-    paddle.drawRect(0, 0, 25, 100);
-    paddle.endFill();
+    var paddle = game.add.bitmapData(25, 100);
+    paddle.ctx.rect(0, 0, 25, 100);
+    paddle.ctx.fillStyle = color;
+    paddle.ctx.fill();
 
-    return paddle;
+    var paddle_sprite = physics_elements.create(x, y, paddle);
+    game.physics.arcade.enable(paddle_sprite);
+    paddle_sprite.body.enable = true;
+    paddle_sprite.body.setSize(25,100);
+
+    return paddle_sprite;
 }
 
 
 function create_horizontal_border(y)
 {
-    var border = game.add.graphics(0,y);
-    border.beginFill(0xAAAAAA, 1);
-    border.lineStyle(2, 0xFFFFFF, 1);
-    border.drawRect(0, 0, 800, 20);
-    border.endFill();
+    var border = game.add.bitmapData(800, 20);
+    border.ctx.rect(0, 0, 800, 20);
+    border.ctx.fillStyle = '#AAAAAA';
+    border.ctx.fill();
 
-    return border;
+    var border_sprite = physics_elements.create(0, y, border);
+    game.physics.arcade.enable(border_sprite);
+    border_sprite.body.enable = true;
+    border_sprite.body.setSize(800,20);
+    border_sprite.body.immovable = true;
+
+    return border_sprite;
 }
