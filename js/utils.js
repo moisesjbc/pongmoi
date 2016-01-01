fdf54028d61b2db829971594be3f4bf46cd87f39
Moisés J. Bonilla Caraballo <moisesjbc@gmail.com>
2016-01-01 13:11:27 +0000

    Changed hover style for buttons.

--- js/utils.js
+++ js/utils.js
@@ -6,11 +6,15 @@ function create_button(game_state, button_text, y, callback)
     button_bitmap.ctx.fillStyle = '#FFFFFF';
     button_bitmap.ctx.fill();
 
+    var normal_style = { fontSize: '16px', fill: '#000000' };
+    var hover_style = { fontSize: '16px', fill: '#999999' };
+    
+
     // Create the button sprite.
     var button = game_state.add.sprite((game.width - 300) / 2.0, y, button_bitmap);
 
     // Create the button label.
-    var button_label = game_state.add.text( 0, 0, button_text, { fontSize: '16px', fill: '#0' });
+    var button_label = game_state.add.text( 0, 0, button_text, normal_style);
     button.addChild(button_label);
 
     // Center label inside the button
@@ -20,8 +24,8 @@ function create_button(game_state, button_text, y, callback)
     // Enables all kind of input actions on this sprite (click, etc)
     button.inputEnabled = true;
 
-    button.events.onInputOver.add(function(){button_label.fontSize = '18px';}, this);
-    button.events.onInputOut.add(function(){button_label.fontSize = '16px';}, this);
+    button.events.onInputOver.add(function(){button_label.setStyle(hover_style);}, this);
+    button.events.onInputOut.add(function(){button_label.setStyle(normal_style);}, this);
     button.events.onInputDown.add(callback, this);
 
     return button;
